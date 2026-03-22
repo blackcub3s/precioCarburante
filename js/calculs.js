832eccf3818876c4b511e9c7d306e50624dd3d03
blackcub3s <santiago.sanchez.sans.44@gmail.com>
2026-03-22 12:42:45 +0100

    afegit variació de IEH produida pel real decret 21 març 2026
    git branch

--- js/calculs.js
+++ js/calculs.js
@@ -20,30 +20,38 @@ document.addEventListener("DOMContentLoaded", () => {
 });
 
 //PRE: carburant: es D o G
-//.    preuDiaPrevi_21PerCent: es el preu que tenia el carburant el dia 21 de març, abans de la pujada de l'iva
+//     preuDiaPrevi_21PerCent: es el preu que tenia el carburant el dia 21 de març, abans de la pujada de l'iva
 //POST: s'emplena el
 function emplenaSortidaPreu_iva10percent(carburant, preuDiaPrevi_21PerCent) {
     const dom10perCent = document.getElementById("sortidaDades10perCent");
     if (carburant === "D") {
-        let IEH_diesel = 0.378; 
-        dom10perCent.textContent = rebaixaPreuCombustible_21_a_10(IEH_diesel, preuDiaPrevi_21PerCent);
+        let IEH_previ_diesel = 0.378; // -- IEH ESTIMAT vigent abans de l'entrada del real decret 21 març 2026
+        let IEH_nou_diesel = 0.26731; //segons real decret 21 març 2026 --> https://www.boe.es/boe/dias/2026/03/21/pdfs/BOE-A-2026-6544.pdf --> pág. 4338 --> epígrafe 1.3
+        dom10perCent.textContent = rebaixaPreuCombustible_21_a_10(IEH_previ_diesel, preuDiaPrevi_21PerCent, IEH_nou_diesel);
     } else if (carburant === "G") {
-        let IEH_gasolina = 0.472;
-        dom10perCent.textContent = rebaixaPreuCombustible_21_a_10(IEH_gasolina, preuDiaPrevi_21PerCent);
+        let IEH_previ_gasolina = 0.472; //-- IEH ESTIMAT vigent abans de l'entrada del real decret 21 març 2026
+        let IEH_nou_gasolina95 = 0.30432; //segons real decret 21 març 2026 --> https://www.boe.es/boe/dias/2026/03/21/pdfs/BOE-A-2026-6544.pdf --> pág. 4338 --> epígrafe 1.2.2
+        dom10perCent.textContent = rebaixaPreuCombustible_21_a_10(IEH_previ_gasolina, preuDiaPrevi_21PerCent, IEH_nou_gasolina95);
     }
 }
+/*
+RAONAMENTS PREVIS: 
+Obtinc el preu base de la gasolina el dia 21 de març amb IEH previ (més car) i amb iva a 21%:
+       EXPR 1    (x + 0,378)*1.21 = 1.65  ---*-->   (preuBase + IEH_previ)*1.21 = preuSurtidorPrevi          
+Obtinc el preu base de la gasolina que hi hauria d'haver el 22 de març amb el nou IEH i l'iva al 10%:
+       EXPR 2:   (x + 0,26731)*1.10 = Y   ---*-->   (preuBase + IEH_nou)*1.10 = preuSurtidorActual.    
+        --------------------------------------------------------------------
+        --  *NOTA: x es preu base || Y es preu al surtidor amb 10% de iva --
+        --------------------------------------------------------------------
 
-
-//RAONAMENTS PREVIS: 
-//EXPRESSIO 1 CAS PARTICULAR: (x + 0,379)*1.21 = 1.65 --> (preuBase + IEH)*1.21 = preuSurtidorPrevi       [amb 21% iva]
-//EXPRESSIO 2 CAS PARTICULAR: (x + 0,379)*1.10 = Y -----> (preuBase + IEH)*1.10 = preuSurtidorActual.     [amb 10% iva]
-//x es preu base || Y es preu al surtidor amb 10% de iva
-
-//PRE: IEH --> impost del carburant (diferent entre diesel i gasolina)
-//.    preuSurtidorPrevi --> el preu que veiem a surtidor el dia previ a la baixada de l'iva (21 de març)
-//POST: preuSurtidorActual --> el preu que HAURIEM DE VEURE en surtidor el dia posterior a la baixada de l'iva al 10% (22 de març)
-function rebaixaPreuCombustible_21_a_10(IEH, preuSurtidorPrevi) {
-    let preuBase = (preuSurtidorPrevi - 1.21*IEH)/1.21;      //expressio 1 aillada per trobar preuBase (la X d'expressio 1)
-    let preuSurtidorActual = (preuBase + IEH)*1.10;
+PRE: 
+     - IEH_PREVI --> impost del carburant (diferent entre diesel i gasolina) abans del RD 21 de març 2026
+     - preuSurtidorPrevi --> el preu que veiem a surtidor el dia previ a la baixada de l'IVA i IEH (21 de març)
+POST: 
+     - preuSurtidorActual --> el preu que HAURIEM DE VEURE en surtidor el dia posterior a la baixada de l'iva al 10% i IEH (22 de març).
+*/
+function rebaixaPreuCombustible_21_a_10(IEH_previ, preuSurtidorPrevi, IEH_nou) {
+    let preuBase = (preuSurtidorPrevi - 1.21*IEH_previ)/1.21;      //EXPR 1 aillada per trobar preuBase (la X d'EXPR 1)
+    let preuSurtidorActual = (preuBase + IEH_nou)*1.10;
     return preuSurtidorActual; 
 }
